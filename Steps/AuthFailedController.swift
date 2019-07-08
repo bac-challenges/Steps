218334ad841c8ec83de6d3efc9ab5dc2b5842d43
emile <emile@blueantcorp.com>
2019-07-08 19:45:50 +0300

    Add HealthKit AuthFailed screen

--- Steps/AuthFailedController.swift
+++ Steps/AuthFailedController.swift
@@ -33,21 +33,93 @@ import UIKit
 
 class AuthFailedController: UIViewController {
 
+	// Model
+	private lazy var model = StepsViewModel()
+	
+	// UI
+	private lazy var titleLabel: UILabel = {
+		let label = UILabel()
+		label.textAlignment = .center
+		label.textColor = .white
+		label.text = model.authFailedTitleText
+		label.numberOfLines = 0
+		label.font = .systemFont(ofSize: 18, weight: .heavy)
+		return label
+	}()
+	
+	private lazy var detailLabel: UILabel = {
+		let label = UILabel()
+		label.textAlignment = .center
+		label.textColor = .white
+		label.text = model.authFailedDetailText
+		label.alpha = 0.5
+		label.font = .systemFont(ofSize: 14, weight: .medium)
+		return label
+	}()
+	
+	private lazy var button: UIButton = {
+		let button = UIButton()
+		button.layer.borderWidth = 1
+		button.layer.borderColor = UIColor.white.cgColor
+		button.setTitle(model.authFailedButtonTitleText, for: .normal)
+		button.setTitleColor(.white, for: .normal)
+		button.addTarget(self, action: #selector(openHealthApp), for: .touchUpInside)
+		return button
+	}()
+	
+	private lazy var iconView: UIImageView = {
+		let imageView = UIImageView("heart")
+		imageView.tintColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+		return imageView
+	}()
+	
+	// Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		setupView()
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - Actions
+extension AuthFailedController {
+	@objc func openHealthApp() {
+		guard let settingsUrl = URL(string: "x-apple-health://") else {
+			return
+		}
+		
+		if UIApplication.shared.canOpenURL(settingsUrl) {
+			UIApplication.shared.open(settingsUrl) { success in
+				print("Settings opened: \(success)")
+			}
+		}
+	}
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+// MARK: - UI
+extension AuthFailedController {
+	func setupView() {
+		view.addSubview(titleLabel)
+		view.addSubview(detailLabel)
+		view.addSubview(button)
+		view.addSubview(iconView)
+		
+		titleLabel.anchor(centerX: view.centerXAnchor,
+						  centerY: view.centerYAnchor,
+						  paddingCenterY: -80)
+		
+		detailLabel.anchor(top: titleLabel.bottomAnchor,
+						   paddingTop: 5,
+						   centerX: view.centerXAnchor)
+		
+		button.anchor(bottom: view.bottomAnchor,
+					  paddingBottom: 40,
+					  left: detailLabel.leftAnchor,
+					  right: detailLabel.rightAnchor)
+		
+		iconView.anchor(bottom: titleLabel.topAnchor,
+						right: titleLabel.rightAnchor,
+						paddingRight: -13,
+						width: 30,
+						height: 30)
+	}
 }
