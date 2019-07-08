cb21fcae7be33efcec055eae10dd8b8b08f881a3
emile <emile@blueantcorp.com>
2019-07-08 16:25:16 +0300

    Select view based on HealthKit permissions

--- Steps/AppDelegate.swift
+++ Steps/AppDelegate.swift
@@ -36,8 +36,6 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
-	
-	private lazy var stepsController = UINavigationController(rootViewController: StepsController())
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
@@ -46,45 +44,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 			
 			// Setup views
 			DispatchQueue.main.sync {
+				
+				// RootViewController
+				let rootViewController = success ? StepsController() : AuthFailedController()
+				
 				self.window = UIWindow(frame: UIScreen.main.bounds)
 				self.window?.makeKeyAndVisible()
-				self.window?.rootViewController = success ? self.stepsController : AuthFailedController()
+				self.window?.rootViewController = UINavigationController(rootViewController: rootViewController)
 			}
 		}
 		return true
 	}
 
 	func applicationWillTerminate(_ application: UIApplication) {
-		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-		// Saves changes in the application's managed object context before the application terminates.
 		self.saveContext()
 	}
 
 	// MARK: - Core Data stack
 	lazy var persistentContainer: NSPersistentContainer = {
-	    /*
-	     The persistent container for the application. This implementation
-	     creates and returns a container, having loaded the store for the
-	     application to it. This property is optional since there are legitimate
-	     error conditions that could cause the creation of the store to fail.
-	    */
 	    let container = NSPersistentContainer(name: "Steps")
-	    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-	        if let error = error as NSError? {
-	            // Replace this implementation with code to handle the error appropriately.
-	            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-	             
-	            /*
-	             Typical reasons for an error here include:
-	             * The parent directory does not exist, cannot be created, or disallows writing.
-	             * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-	             * The device is out of space.
-	             * The store could not be migrated to the current model version.
-	             Check the error message to determine what the actual problem was.
-	             */
-	            fatalError("Unresolved error \(error), \(error.userInfo)")
-	        }
-	    })
+		container.loadPersistentStores { (storeDescription, error) in
+			if let error = error as NSError? {
+				fatalError("Unresolved error \(error), \(error.userInfo)")
+			}
+		}
 	    return container
 	}()
 
@@ -95,8 +78,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	        do {
 	            try context.save()
 	        } catch {
-	            // Replace this implementation with code to handle the error appropriately.
-	            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 	            let nserror = error as NSError
 	            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
 	        }
