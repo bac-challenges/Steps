2225f11aefe3a94f8e3022a297a71e57878d0084
emile <emile@blueantcorp.com>
2019-07-09 00:18:36 +0300

    Add core data manager

--- Steps/Service/CoreDataManager.swift
+++ Steps/Service/CoreDataManager.swift
@@ -30,7 +30,33 @@
 //
 
 import Foundation
+import CoreData
 
-struct CoreDataManager {
+class CoreDataManager {
+	
+	public static let shared = CoreDataManager()
+	
+	lazy var persistentContainer: NSPersistentContainer = {
+		let container = NSPersistentContainer(name: "Steps")
+		container.loadPersistentStores { (storeDescription, error) in
+			if let error = error as NSError? {
+				fatalError("Unresolved error \(error), \(error.userInfo)")
+			}
+		}
+		return container
+	}()
+	
+	// Core Data Saving support
+	func saveContext () {
+		let context = persistentContainer.viewContext
+		if context.hasChanges {
+			do {
+				try context.save()
+			} catch {
+				let nserror = error as NSError
+				fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+			}
+		}
+	}
 	
 }
