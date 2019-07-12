944683242e23ac73f340ed4067d4eb6fde7b12f6
emile <emile@blueantcorp.com>
2019-07-12 12:44:36 +0300

    Change sample range

--- Sources/Service/HealthKitManager.swift
+++ Sources/Service/HealthKitManager.swift
@@ -212,7 +212,7 @@ extension HealthKitManager {
 		var result = [SampleData]()
 		
 		while date <= endDate {
-			let steps = Double(Int.random(in: 0...2000))
+			let steps = Double(Int.random(in: 0...1000))
 			result.append(SampleData(steps: steps,
 									 startDate: date,
 									 endDate: endDate))
